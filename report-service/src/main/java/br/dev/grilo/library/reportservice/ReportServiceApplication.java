d243ea7c16218ad5bec77cf6834d1ba232d00a5f
Matheus Grilo <matheusgrilo@live.com>
2025-04-04 14:52:31 -0300

    Add initial service configurations and application classes for Book, Loan, User, Report, and Notification services

---
+++ report-service/src/main/java/br/dev/grilo/library/reportservice/ReportServiceApplication.java
@@ -0,0 +1,13 @@
+package br.dev.grilo.library.reportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportServiceApplication.class, args);
+	}
+
+}
